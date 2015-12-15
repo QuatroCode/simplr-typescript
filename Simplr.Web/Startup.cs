1553cbb8b7034dd378f31b628f6df882eecf98dd
Giedrius Grabauskas <gied.grabauskas@gmail.com>
2015-12-15 20:26:29 +0200

    Init commit.

---
+++ Simplr.Web/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace Simplr.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+
+            app.Run(async ctx =>
+            {
+                if (!ctx.Request.Path.Value.Contains("."))
+                {
+                    var indexFile = File.ReadAllText(env.MapPath("index.html"));
+                    await ctx.Response.WriteAsync(indexFile);
+                }
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
